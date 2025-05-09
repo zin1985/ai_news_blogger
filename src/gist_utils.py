17e315239d02f97c3deb23d24056461cc46f44f6
zin1985 <136662625+zin1985@users.noreply.github.com>
2025-05-09 15:28:24 +0900

    Update gist_utils.py

--- src/gist_utils.py
+++ src/gist_utils.py
@@ -2,18 +2,18 @@ import os
 import requests
 
 GIST_ID = os.environ.get("GIST_ID")
-GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+GIST_PAT = os.environ.get("GIST_PAT")
 
 def load_posted_urls_from_gist():
     url = f"https://api.github.com/gists/{GIST_ID}"
-    headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
+    headers = {"Authorization": f"Bearer {GIST_PAT}"}
     res = requests.get(url, headers=headers)
     urls = res.json()["files"]["posted_urls.json"]["content"].splitlines()
     return set(url.strip() for url in urls)
 
 def save_posted_urls_to_gist(posted_urls):
     url = f"https://api.github.com/gists/{GIST_ID}"
-    headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
+    headers = {"Authorization": f"Bearer {GIST_PAT}"}
     data = {
         "files": {
             "posted_urls.json": {
